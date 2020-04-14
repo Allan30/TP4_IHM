85441d0b71543482af67ba71fc161fb0c0953816
Des Courtils <allanthekiller@gmail.com>
2020-04-14 22:15:49 +0200

    Manque plus que le prix tot

--- Nouveau dossier/assets/js/script.js
+++ Nouveau dossier/assets/js/script.js
@@ -34,11 +34,20 @@ function afficher(){
 }
 
 var increment = document.getElementById("number").innerHTML;
+var leContenuDuPanier = [];
 
 function plusOne(){
     event.preventDefault();
 
     increment++;
+
+    document.getElementById("number").innerHTML = increment;
+}
+
+function lessOne(){
+    event.preventDefault();
+
+    increment--;
 
     document.getElementById("number").innerHTML = increment;
 }
@@ -49,16 +58,71 @@ for(var element of elements){
     element.addEventListener('click', function(event){
         event.preventDefault();
         plusOne();
-        ajouterAuPanier(event.target.parentNode);
+        console.log(event.target.parentNode.dataset.id);
+        ajouterAuPanier(event.target.parentNode.getElementsByClassName('title')[0],event.target.parentNode.dataset.price,event.target.parentNode.dataset.id);
       });
 }
 
 
 var panier = document.getElementById("panier");
+var prixTot = document.createElement("integer");
+panier.appendChild(prixTot);
 
-function ajouterAuPanier(element){
+function ajouterAuPanier(nom,prix,id){
     event.preventDefault();
-    panier.appendChild(element);
+    
+    if(leContenuDuPanier.find(elementCourantDuTableau => elementCourantDuTableau[0] === id)){
+        leContenuDuPanier.find(elementCourantDuTableau => elementCourantDuTableau[0] === id)[1].innerHTML++;
+        prixTot.innerHTML += parseInt(prix);
+        return;
+    }
+    
+        var compteur = document.createElement("integer");
+        var item = document.createElement('li');
+        var pl = document.createElement("BUTTON");
+        var pltxt = document.createTextNode("+");
+        var mo = document.createElement("BUTTON");
+        var motxt = document.createTextNode("-");
+
+        prixTot.innerHTML += parseInt(prix);
+
+        pl.appendChild(pltxt);
+        mo.appendChild(motxt);
+
+        compteur.innerHTML = 1;
+        item.textContent = nom.textContent + " : " + prix + "€ \n nombre d'unité(s) : ";
+        item.appendChild(compteur);
+
+        panier.appendChild(item);
+        panier.appendChild(mo);
+        panier.appendChild(pl);
+
+        pl.addEventListener('click', function(event){
+            event.preventDefault();
+            plusOne();
+            prixTot.innerHTML += parseInt(prix);
+            leContenuDuPanier.find(elementCourantDuTableau => elementCourantDuTableau[0] === id)[1].innerHTML++;
+            });
+
+        mo.addEventListener('click', function(event){
+            event.preventDefault();
+            if(leContenuDuPanier.find(elementCourantDuTableau => elementCourantDuTableau[0] === id)[1].innerHTML > 1){
+                lessOne();
+                leContenuDuPanier.find(elementCourantDuTableau => elementCourantDuTableau[0] === id)[1].innerHTML--;
+                prixTot.innerHTML -= parseInt(prix);
+            }
+            else{
+                lessOne();
+                leContenuDuPanier.find(elementCourantDuTableau => elementCourantDuTableau[0] === id)[1].innerHTML--;
+                prixTot.innerHTML -= parseInt(prix);
+                panier.removeChild(item);
+                panier.removeChild(mo);
+                panier.removeChild(pl);
+                leContenuDuPanier.pop(leContenuDuPanier.find(elementCourantDuTableau => elementCourantDuTableau[0] === id));
+            }
+            });
+
+        leContenuDuPanier.push([id,compteur]);
 
 }
 
